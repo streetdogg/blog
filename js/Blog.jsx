d74d56a64cbc9676c562b02e9b51b0e788d1d3e1
Piyush Itankar <pitankar@gmail.com>
2019-04-09 22:37:37 +0530

    Adds paper view and sample post
    Signed-off-by: Piyush Itankar <pitankar@gmail.com>

--- js/Blog.jsx
+++ js/Blog.jsx
@@ -4,31 +4,70 @@ class Blog extends React.Component {
         this.state = {
             loaded: false,
             posts: [],
+            postUrl: "",
+            blog: "",
         }
     }
 
-    fetchPosts() {
-        fetch('data/blog_preview.json')
+    fetchPosts(url) {
+        fetch(url)
         .then(res => res.json())
         .then(json => {
             this.setState({
                 loaded: true,
                 posts : json,
             })
-
         });
     }
 
+    renderDetails = (id)=>{
+        var post_url = "data/blog/post-" + id + ".json";
+        console.log(post_url)
+        this.setState({
+            postUrl : post_url,
+        })
+    }
+
+    loadHome() {
+        this.setState({
+            postUrl: "",
+            blog: ""
+        })
+    }
+
     render() {
-        var {loaded, posts} = this.state;
+        var {loaded, posts, postUrl, blog} = this.state;
+
+        if (postUrl != "") {
+            fetch(postUrl)
+            .then(res => res.text())
+            .then(json => {
+                console.log(json)
+                this.setState({
+                    blog: json,
+                });
+            });
+        }
+
+        if (blog != "") {
+            return (
+                <div className="paper" onClick={this.loadHome.bind(this)}>
+                    <div class="md" dangerouslySetInnerHTML={{__html: this.state.blog}} />
+                </div>
+            )
+        }
 
         if (!loaded) {
-            this.fetchPosts()
-            return <div>Loading....</div>
+            var url = 'data/blog_preview.json'
+            this.fetchPosts(url)
+            return     <div class="footer">
+            Under active construction. Maintained and developed by Piyush Itankar.
+        </div>
         } else {
+            console.log("rendering blog")
             return posts.map(post => {
                 return (
-                    <PostCard key={post.id} post={post}/>
+                    <PostCard key={post.id} cb={this.renderDetails} post={post}/>
                 );
             });
         }
