4894150b6d0347599daadf3c4d6d990ad86ed1ed
Piyush Itankar <pitankar@gmail.com>
2019-04-10 18:22:46 +0530

    Corrects the blog loading and adds the following: 1. A back button to return to the home page. 2. Cosmetic changes to adjust as per the viewport width. 3. Ported many blogs from the original site to here. 4. Adds features for progressive web app enablement.
    Signed-off-by: Piyush Itankar <pitankar@gmail.com>

--- js/Blog.jsx
+++ js/Blog.jsx
@@ -5,7 +5,7 @@ class Blog extends React.Component {
             loaded: false,
             posts: [],
             postUrl: "",
-            blog: "",
+            blogLoaded: false
         }
     }
 
@@ -20,39 +20,56 @@ class Blog extends React.Component {
         });
     }
 
+    copyright() {
+        return "© 2019 PI's Blog. Owned and Operated by Piyush Itankar."
+    }
+
     renderDetails = (id)=>{
         var post_url = "data/blog/post-" + id + ".json";
-        console.log(post_url)
         this.setState({
             postUrl : post_url,
         })
     }
 
-    loadHome() {
+    loadHome = () => {
         this.setState({
             postUrl: "",
-            blog: ""
+            blogLoaded: false,
         })
     }
 
     render() {
-        var {loaded, posts, postUrl, blog} = this.state;
+        var {loaded, posts, postUrl, blogLoaded} = this.state;
 
-        if (postUrl != "") {
+        if (postUrl != "" && blogLoaded == false) {
             fetch(postUrl)
             .then(res => res.text())
-            .then(json => {
-                console.log(json)
+            .then(post => {
                 this.setState({
-                    blog: json,
+                    blog: post,
+                    blogLoaded: true,
                 });
             });
+            return (
+                <div className="ft">
+                    Loading...
+                </div>)
         }
 
-        if (blog != "") {
+        if (blogLoaded) {
             return (
-                <div className="paper" onClick={this.loadHome.bind(this)}>
-                    <div class="md" dangerouslySetInnerHTML={{__html: this.state.blog}} />
+                <div className="paper">
+                    <div className="md">
+                        <div dangerouslySetInnerHTML={{__html: this.state.blog}}></div>
+                        <div className="st" onClick={this.loadHome.bind(this)}>
+                            <div className="pointer">
+                            <i className="fa fa-arrow-left"></i>
+                            </div>
+                        </div>
+                        <div className="ft">
+                            {this.copyright()}
+                        </div>
+                    </div>
                 </div>
             )
         }
@@ -60,11 +77,11 @@ class Blog extends React.Component {
         if (!loaded) {
             var url = 'data/blog_preview.json'
             this.fetchPosts(url)
-            return     <div class="footer">
-            Under active construction. Maintained and developed by Piyush Itankar.
-        </div>
+            return (
+                <div className="ft">
+                    Loading...
+                </div>)
         } else {
-            console.log("rendering blog")
             return posts.map(post => {
                 return (
                     <PostCard key={post.id} cb={this.renderDetails} post={post}/>
